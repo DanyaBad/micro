8ea56766e5fe8f833c64caa5aac7f836d7929709
DanyaBad <wmcrx@mail.ru>
2024-11-02 16:42:55 +1000

    test

---
+++ WebApplication1/WebApplication1/Controllers/UsersConroller.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UserAdminService.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UserAdminService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private static List<User> Users = new List<User>();
+        private static int nextId = 1;
+
+        [HttpGet]
+        public ActionResult<IEnumerable<User>> GetUsers()
+        {
+            return Ok(Users);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<User> GetUser(int id)
+        {
+            var user = Users.FirstOrDefault(u => u.Id == id);
+            if (user == null) return NotFound();
+            return Ok(user);
+        }
+
+        [HttpPost]
+        public ActionResult<User> CreateUser([FromBody] User user)
+        {
+            if (user == null) return BadRequest("User cannot be null.");
+            user.Id = nextId++;
+            Users.Add(user);
+            return CreatedAtAction(nameof(GetUser), new { id = user.Id }, user);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult UpdateUser(int id, [FromBody] User updatedUser)
+        {
+            var user = Users.FirstOrDefault(u => u.Id == id);
+            if (user == null) return NotFound();
+
+            user.Name = updatedUser.Name;
+            user.Email = updatedUser.Email;
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult DeleteUser(int id)
+        {
+            var user = Users.FirstOrDefault(u => u.Id == id);
+            if (user == null) return NotFound();
+
+            Users.Remove(user);
+            return NoContent();
+        }
+    }
+}
